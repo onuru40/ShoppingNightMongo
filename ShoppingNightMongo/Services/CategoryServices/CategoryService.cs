b48b05ada4f698043174e2ee73e03a790a924ac7
onur_unlu40 <onur_unlu40@hotmail.com>
2025-05-15 21:57:52 +0300

    CRUD işlemleri

--- ShoppingNightMongo/Services/CategoryServices/CategoryService.cs
+++ ShoppingNightMongo/Services/CategoryServices/CategoryService.cs
@@ -12,19 +12,20 @@ namespace ShoppingNightMongo.Services.CategoryServices
         private readonly IMongoCollection<Category> _categoryCollection;
         public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
         {
-            var client = new MongoClient(_databaseSettings.DatabaseName);
+            var client = new MongoClient(_databaseSettings.ConnectionString);
             var database = client.GetDatabase(_databaseSettings.DatabaseName);
             _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
             _mapper = mapper;
         }
-        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
         {
-            throw new NotImplementedException();
+            var value = _mapper.Map<Category>(createCategoryDto);
+            await _categoryCollection.InsertOneAsync(value);
         }
 
-        public Task DeleteCategoryAsync(string id)
+        public async Task DeleteCategoryAsync(string id)
         {
-            throw new NotImplementedException();
+            await _categoryCollection.DeleteOneAsync(id);
         }
 
         public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
@@ -37,9 +38,10 @@ namespace ShoppingNightMongo.Services.CategoryServices
             throw new NotImplementedException();
         }
 
-        public Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
         {
-            throw new NotImplementedException();
+            var value = _mapper.Map<Category>(updateCategoryDto);
+            await _categoryCollection.FindOneAndReplaceAsync(x=>x.CategoryId==updateCategoryDto.CategoryId, value);
         }
     }
 }
--- ShoppingNightMongo/Services/ProductServices/ProductService.cs
+++ ShoppingNightMongo/Services/ProductServices/ProductService.cs
@@ -1,17 +1,34 @@
-﻿using ShoppingNightMongo.Dtos.ProductDtos;
+﻿using AutoMapper;
+using MongoDB.Driver;
+using ShoppingNightMongo.Dtos.CustomerDtos;
+using ShoppingNightMongo.Dtos.ProductDtos;
+using ShoppingNightMongo.Entities;
+using ShoppingNightMongo.Settings;
 
 namespace ShoppingNightMongo.Services.ProductServices
 {
     public class ProductService : IProductService
     {
-        public Task CreateProductAsync(CreateProductDto createProductDto)
+        private readonly IMapper _mapper;
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettings)
         {
-            throw new NotImplementedException();
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+            _mapper = mapper;
         }
 
-        public Task DeleteProductAsync(string id)
+        public async Task CreateProductAsync(CreateProductDto createProductDto)
         {
-            throw new NotImplementedException();
+            var value = _mapper.Map<Product>(createProductDto);
+            await _productCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteProductAsync(string id)
+        {
+            await _productCollection.DeleteOneAsync(x=> x.ProductId == id);
         }
 
         public Task<List<ResultProductDto>> GetAllProductAsync()
@@ -24,9 +41,10 @@ namespace ShoppingNightMongo.Services.ProductServices
             throw new NotImplementedException();
         }
 
-        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        public async Task UpdateProductAsync(UpdateProductDto updateProductDto)
         {
-            throw new NotImplementedException();
+            var value = _mapper.Map<Product>(updateProductDto);
+            await _productCollection.FindOneAndReplaceAsync(x => x.ProductId == updateProductDto.ProductId, value);
         }
     }
 }
