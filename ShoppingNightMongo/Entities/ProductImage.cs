bafd64909cff4c21102b9ad4496d4a08cace7b7f
onur_unlu40 <onur_unlu40@hotmail.com>
2025-06-01 12:33:10 +0300

    Yeni eklemeler

---
+++ ShoppingNightMongo/Entities/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ShoppingNightMongo.Entities
+{
+    public class ProductImage
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductImageId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ShoppingNightMongo/Entities/Slider.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ShoppingNightMongo.Entities
+{
+    public class Slider
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string SliderId { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
